9308c3618fc4eb4b41ed441fb77e8799321effd8
Shreyansh Pandey <shreyanshpandey2003@gmail.com>
2026-01-13 11:55:31 +0530

    Updated Frame Count in ScrollyCanvas

--- app/components/ScrollyCanvas.tsx
+++ app/components/ScrollyCanvas.tsx
@@ -3,7 +3,7 @@
 import { useScroll, useMotionValueEvent } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 
-const FRAME_COUNT = 135;
+const FRAME_COUNT = 150;
 
 export default function ScrollyCanvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
